da7a3f61b8941430880aba541dae5493498a67e4
LemonYangZW <36176863+LemonYangZW@users.noreply.github.com>
2018-05-06 08:47:08 +0800

    上传项目

---
+++ src/com/sangebang/water/domain/Voter.java
@@ -0,0 +1,11 @@
+package com.sangebang.water.domain;
+
+import java.sql.Timestamp;
+
+public class Voter {
+	private int id;	
+	private String tid; 			//任务编号
+	private String wxname;		//微信openid
+	private Timestamp tm;		//接单时间
+	private String path;				//图片链接
+}
